f319a9c87e97c198080060bb3524ee396357f83d
Peyton Casper <peytoncas@gmail.com>
2017-08-31 17:04:10 -0500

    Modfied load_schema to work with 5.0 init error in DSE Graph

--- resources/graph/load_schema.groovy
+++ resources/graph/load_schema.groovy
@@ -1,3 +1,8 @@
+:remote close
+:remote connect tinkerpop.server conf/remote.yaml session-managed
+:remote config timeout max
+:remote console
 system.graph('powertrain_graph').ifNotExists().create()
 :remote config alias g powertrain_graph.g
 :load /tmp/Powertrain2/resources/graph/powertrain_graph.groovy
+:remote close
