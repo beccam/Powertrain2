c8412a36cbdbf8f3f3bc1718637452624a27a4d7
Rebecca Mills <beccamills86@gmail.com>
2020-05-24 09:11:17 -0230

    Update load_schema.groovy

--- resources/graph/load_schema.groovy
+++ resources/graph/load_schema.groovy
@@ -2,7 +2,7 @@
 :remote connect tinkerpop.server conf/remote.yaml session-managed
 :remote config timeout max
 :remote console
-system.graph('powertrain_graph').engine(Classic).ifNotExists().create()
+system.graph('powertrain_graph').engine(Classic).create()
 :remote config alias g powertrain_graph.g
 :load /tmp/Powertrain2/resources/graph/powertrain_graph.groovy
 :remote close
